9e42459eb191d6d79fe0e3e98746baa81ce90c85
nevarwin <ravencsolis@gmail.com>
2025-08-04 21:50:45 +0800

    fixed the all, year and added a new view

---
+++ ExpenseTracker/ExpenseTracker/BudgetViewController.h
@@ -0,0 +1,16 @@
+//
+//  BudgetViewController.h
+//  ExpenseTracker
+//
+//  Created by raven on 8/4/25.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BudgetViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
