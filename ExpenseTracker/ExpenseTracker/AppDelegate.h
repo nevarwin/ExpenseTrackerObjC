3ec9af6d2cf06e731d75cfed820b611e2a2ff874
nevarwin <ravencsolis@gmail.com>
2025-08-04 22:43:58 +0800

    fix the default index of uisegment

--- ExpenseTracker/ExpenseTracker/AppDelegate.h
+++ ExpenseTracker/ExpenseTracker/AppDelegate.h
@@ -12,6 +12,7 @@
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (readonly, strong) NSPersistentContainer *persistentContainer;
+@property (strong, nonatomic) UIWindow *window;
 
 - (void)saveContext;
 
