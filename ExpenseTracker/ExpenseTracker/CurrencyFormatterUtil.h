1d06caf73c81506ba9629cbd3b671c365d878877
nevarwin <ravencsolis@gmail.com>
2025-10-07 17:13:54 +0800

    feat: create currency formatter

---
+++ ExpenseTracker/ExpenseTracker/CurrencyFormatterUtil.h
@@ -0,0 +1,15 @@
+//
+//  CurrencyFormatterUtil.h
+//  ExpenseTracker
+//
+//  Created by raven on 10/7/25.
+//
+
+#ifndef CurrencyFormatterUtil_h
+#define CurrencyFormatterUtil_h
+
+@interface CurrencyFormatterUtil : NSObject
++ (NSNumberFormatter *)currencyFormatter;
+@end
+
+#endif /* CurrencyFormatterUtil_h */
