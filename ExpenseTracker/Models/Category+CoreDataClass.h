b9d2d65f5540ffba8e2c55ddab4dd031adb8897d
nevarwin <ravencsolis@gmail.com>
2026-01-06 22:30:50 +0800

    fix: change category to manual and core model target

---
+++ ExpenseTracker/Models/Category+CoreDataClass.h
@@ -0,0 +1,22 @@
+//
+//  Category+CoreDataClass.h
+//  ExpenseTracker
+//
+//  Created by raven on 1/6/26.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class Budget, Transaction;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Category : NSManagedObject
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#import "Category+CoreDataProperties.h"
